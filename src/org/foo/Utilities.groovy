af1d66adbc69c7da67fc2e278605fa7f9b3c8421
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-27 21:29:08 -0300

    fazendo o primeiro teste

---
+++ src/org/foo/Utilities.groovy
@@ -0,0 +1,13 @@
+package org.foo 
+
+class Utilities implements Serializable{
+    def steps 
+    
+    Utilities(steps){
+        this.steps = steps; 
+    }
+
+    def mvn(args){
+        steps.echo "${args} --- "
+    }
+}
