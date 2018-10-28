29b4719d51727edae2b996d37085048feb4d149c
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-28 10:37:10 -0300

    colocando def

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,11 +1,11 @@
 package org.foo 
 
-class Utilities implements Serializable{
-    def steps 
+class Utilities /*implements Serializable*/{
+    /*def steps 
     
     Utilities(steps){
         this.steps = steps; 
-    }
+    }*/
 
     /*def mvn(args){
         steps.echo "${args} --- "
